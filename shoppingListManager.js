03661ea48b0088b2fe2074fab60ebac6489118de
MarcmotenDC <superman868789@gmail.com>
2024-11-27 11:37:06 -0600

    Add functions for shopping list

--- shoppingListManager.js
+++ shoppingListManager.js
@@ -1,1 +1,29 @@
+let shoppingList = []
 
+const addItem = (item) => {
+    if (typeof item === 'string' && item.trim()) {
+        shoppingList.push(item.trim())
+    }
+}
+
+//Can filter by item value and index
+const removeItem = (item) => {
+    if (typeof item === 'string') {
+        shoppingList = shoppingList.filter(value => value !== item)
+    } else {
+        if (item >= 0 && item < shoppingList.length) {
+            shoppingList.splice(item, 1);
+        }
+    }
+
+}
+
+const getItems = () => {
+    return shoppingList
+}
+
+const clearList = () => {
+    shoppingList = []
+}
+
+module.exports = { addItem, removeItem, getItems, clearList }
